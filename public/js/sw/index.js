d112051501308c653f7d5c4e4ac56301a222c309
Rob-Rychs <Robrychel@gmail.com>
2017-12-01 16:52:18 -0800

    add custom response for 404 page and a custom response for failed network request (offline-first)

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,20 @@
 self.addEventListener('fetch', (event) => { 
-  if (event.request.url.endsWith('.jpg')) {
-    event.respondWith(
-      fetch('/imgs/dr-evil.gif')
-    );
-  }
+  // dr-evil image hijack
+  // if (event.request.url.endsWith('.jpg')) {
+  //   event.respondWith(
+  //     fetch('/imgs/dr-evil.gif')
+  //   );
+  // }
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      if (response.status == 404) {
+        return new Response("Whoops, not found");
+      }
+      return response;
+    }).catch(() => {
+      return new Response("Uh oh, that totally failed!");
+    })
+  );
   // new Response('<b class="a-winner-is-me">Hello World</b>', {
   //   headers: {"Content-Type": "text/html"}
   //   })
