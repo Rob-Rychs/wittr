40d69d7eb763aa12066a6d42e744c0844be733fb
Rob-Rychs <Robrychel@gmail.com>
2017-12-01 16:37:12 -0800

    changed sw to hijack all requests for .jpg and respond with dr-evil.gif

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,11 @@
-self.addEventListener('fetch', (event) => { event.respondWith(
-  new Response('<b class="a-winner-is-me">Hello World</b>', {
-    headers: {"Content-Type": "text/html"}
-    })
-  );
+self.addEventListener('fetch', (event) => { 
+  if (event.request.url.endsWith('.jpg')) {
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    );
+  }
+  // new Response('<b class="a-winner-is-me">Hello World</b>', {
+  //   headers: {"Content-Type": "text/html"}
+  //   })
+  // );
 });
