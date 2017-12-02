091f674af6565072fd2a98f67530be07eb692175
Rob-Rychs <Robrychel@gmail.com>
2017-12-01 17:17:50 -0800

    added notes on cache calls

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,9 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    
+  )
+});
+
 self.addEventListener('fetch', (event) => { 
   // dr-evil image hijack
   // if (event.request.url.endsWith('.jpg')) {
@@ -16,7 +22,21 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+// custom new html response code below:
 // new Response('<b class="a-winner-is-me">Hello World</b>', {
   //   headers: {"Content-Type": "text/html"}
   //   })
   // );
+  
+// cache example code below:  
+// caches.open('my-stuff').then(funciton(cache) {
+//   //...
+// });
+// caches.put(request, response);
+// cache.addAll([
+//   '/foo',
+//   '/bar'
+// ]); 
+//cache.addAll is atomic, if any of these fail to cache, they will all fail... checkout https://www.talater.com/adderall/ cache.aderall
+// cache.match(request);
+// caches.match(request);
