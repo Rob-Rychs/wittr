3d3b6ab8e361822af2948921ec249416501e3555
Rob-Rychs <Robrychel@gmail.com>
2017-12-02 12:37:20 -0800

    added a peopleStore to indexedDB and practiced .put and .getAll

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,8 +1,14 @@
 import idb from 'idb';
 
-var dbPromise = idb.open('test-db', 1, function(upgradeDb) {
-  var keyValStore = upgradeDb.createObjectStore('keyval');
-  keyValStore.put("world", "hello");
+// this might be one of the only times you don't use a break statement in a switch... ignore your linter
+var dbPromise = idb.open('test-db', 2, function(upgradeDb) {
+  switch(upgradeDb.oldVersion) {
+    case 0: 
+      var keyValStore = upgradeDb.createObjectStore('keyval');
+      keyValStore.put("world", "hello");
+    case 1: 
+      upgradeDb.createObjectStore('people', { keyPath: 'name' });
+  }
 });
 
 // read "hello" in "keyval"
@@ -34,4 +40,42 @@ dbPromise.then(function(db) {
   return tx.complete;
 }).then(function() {
   console.log('Added favoriteAnimal:cat to keyval');
+});
+
+// create a peopleStore and store some people objects in it
+dbPromise.then(function(db) {
+  var tx = db.transaction('people', 'readwrite');
+  var peopleStore = tx.objectStore('people');
+
+  peopleStore.put({
+    name: 'Sam Munoz',
+    age: 25,
+    favoriteAnimal: 'cat'
+  });
+
+  peopleStore.put({
+    name: 'Raza Malik',
+    age: 27,
+    favoriteAnimal: 'dog'
+  });
+
+  peopleStore.put({
+    name: 'Jake Archibald',
+    age: 33,
+    favoriteAnimal: 'wombat'
+  });
+
+  return tx.complete;
+}).then(function() {
+  console.log('People added');
+});
+
+// lets read from indexedDB by calling .getALL on peopleStore and logging to console
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  var peopleStore = tx.objectStore('people');
+
+  return peopleStore.getAll();
+}).then(function(people) {
+  console.log('People:', people);
 });
